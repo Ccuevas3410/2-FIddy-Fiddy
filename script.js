54401bc242e8a61adf6eeaea2d399de80cbd9de8
Cristian <Ccuevas3410@gmail.com>
2019-09-21 18:17:40 -0400

    More changes

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
-var APIKey = "ecdfe49f601550468d93e457f818f6d04e1bf34dd15a9c725bc16e574f579e71";
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
+
+var APIKey = "ecdfe49f601550468d93e457f818f6d04e1bf34dd15a9c725bc16e574f579e71";
 
 
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
 
 var query = Math.round(Math.random())
+var page = getRandomInt(50)
 
 if (query) {
-  query = "food";
+  query = "food"
 } else {
   query = "puppies"
 }
 
 
-$.getJSON('https://api.unsplash.com/search/photos?page=1&query=' + query + '&client_id=ecdfe49f601550468d93e457f818f6d04e1bf34dd15a9c725bc16e574f579e71', function (data) {
+$.getJSON('https://api.unsplash.com/search/photos?page=' + page + ' &query=' + query + '&client_id=ecdfe49f601550468d93e457f818f6d04e1bf34dd15a9c725bc16e574f579e71', function (data) {
 
-  console.log(data);
+  console.log(data)
 
-  var imageList = data.results;
+  var imageList = data.results
 
 
 
 
   $.each(imageList, function (i, val) {
 
-    var image = val;
-    var imageURL = val.urls.regular;
+    var image = val
+    var imageURL = val.urls.regular
     console.log(imageURL)
 
     const card = document.createElement('div')
-    card.setAttribute('class', 'card')
+    card.setAttribute('class', 'container')
 
 
 
-    var img = document.createElement('img');
-    img.src = imageURL;
+    var img = document.createElement('img')
+    img.src = imageURL
 
-    document.getElementById('div').appendChild(img);
+    div.appendChild(img)
 
 
 
@@ -52,3 +57,7 @@ $.getJSON('https://api.unsplash.com/search/photos?page=1&query=' + query + '&cli
   });
 
 });
+
+
+//TODO : 
+// FIX THE SCRIPT LOADING OF JQUERY
