1be2c53d0981f82bddc821ea9c8204e091d75e0e
Cristian <Ccuevas3410@gmail.com>
2019-09-21 23:42:15 -0400

    More updates

--- script.js
+++ script.js
@@ -6,25 +6,25 @@ function getRandomInt(max) {
 var APIKey = "ecdfe49f601550468d93e457f818f6d04e1bf34dd15a9c725bc16e574f579e71";
 
 
+
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
 
 var query = Math.round(Math.random())
-var page = getRandomInt(50)
+var page = getRandomInt(30)
 
 if (query) {
   query = "kitties"
 } else {
   query = "puppies"
 }
-
-
-let body = document.documentElement;
-
+var APICall = 'https://api.unsplash.com/search/photos?page=' + page + ' &query=' + query + '&client_id=ecdfe49f601550468d93e457f818f6d04e1bf34dd15a9c725bc16e574f579e71'
 
 
 
-$.getJSON('https://api.unsplash.com/search/photos?page=' + page + ' &query=' + query + '&client_id=ecdfe49f601550468d93e457f818f6d04e1bf34dd15a9c725bc16e574f579e71', function (data) {
+
+
+$.getJSON(APICall, function (data) {
 
   console.log(data)
 
@@ -46,7 +46,7 @@ $.getJSON('https://api.unsplash.com/search/photos?page=' + page + ' &query=' + q
     img.src = imageURL
 
     div.appendChild(img)
-
+    $('body').css('background-image', 'url(' + img + ')')
 
 
 
@@ -54,6 +54,8 @@ $.getJSON('https://api.unsplash.com/search/photos?page=' + page + ' &query=' + q
 
 
 });
+
+
 
 
 //TODO : 
