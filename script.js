75d2cfb45321012e9ec7f71d622c93811cc9b567
Cristian <Ccuevas3410@gmail.com>
2019-09-22 14:06:26 -0400

    Aesthetic changes

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ var APIKey = "ecdfe49f601550468d93e457f818f6d04e1bf34dd15a9c725bc16e574f579e71";
 
 
 
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
+
+const container = document.getElementById('container')
+
 
 var query = Math.round(Math.random())
 var page = getRandomInt(30)
@@ -38,21 +39,53 @@ $.getJSON(APICall, function (data) {
     var image = val
     var imageURL = val.urls.regular
 
-    console.log(image)
+    const card = document.createElement('div')
+    card.setAttribute('class', 'box')
+
 
 
 
     var img = document.createElement('img')
     img.src = imageURL
+    img.setAttribute('class', 'center')
 
-    div.appendChild(img)
-    $('body').css('background-image', 'url(' + img + ')')
+    // Append the cards to the container element
+
+
+    container.appendChild(card)
+    card.appendChild(img)
+
+
 
 
 
   });
 
 
+});
+
+
+
+var APIKey = 'ecdfe49f601550468d93e457f818f6d04e1bf34dd15a9c725bc16e574f579e71';
+
+$.getJSON('https://api.unsplash.com/search/photos?query=chicago&per_page=50&client_id=ecdfe49f601550468d93e457f818f6d04e1bf34dd15a9c725bc16e574f579e71', function (data) {
+  console.log(data);
+
+
+  var imageList = data.results;
+
+  $.each(imageList, function (i, val) {
+
+    var image = val;
+    var imageURL = val.urls.regular;
+    var imageWidth = val.width;
+    var imageHeight = val.height;
+
+    if (imageWidth > imageHeight) {
+      $('.grid').append('<div class="image"><img src="' + imageURL + '"></div>');
+    }
+
+  });
 });
 
 
